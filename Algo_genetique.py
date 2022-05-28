386c0aceb95da004070457f50e1e5affb5b3e7fc
Hazem09 <46021657+Hazem09@users.noreply.github.com>
2022-05-28 16:23:38 +0100

    Add files via upload

---
+++ Algo_genetique.py
@@ -0,0 +1,140 @@
+import numpy as np, matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+class Population():
+    '''Une classe où nous pouvons stocker la population,
+    le score de chaque individu, les parents sélectionnés pour le crossover,
+    l'individu avec le meilleur score et la matrice des distances.'''
+    def __init__(self,villes, mat_distance,nbr_population):
+        # initialiser la première population de façon aléatoire  
+        self.sac = np.asarray([np.random.permutation(villes) for _ in range(nbr_population)])
+        self.parents = [] # une liste vide pour stocker les parents
+        self.score = 0 # une variable pour stocker le score des individus 
+        self.meilleur = None # une variable pour stocker le meilleur individu
+        self.mat_distance = mat_distance # une variable pour stocker la matrice des distances
+
+
+    def fitness(self, chromosome):
+        ''''Cette fonction calcule le score des individus.'''
+        return sum(
+            [
+                self.mat_distance[chromosome[i], chromosome[i + 1]]
+                for i in range(len(chromosome) - 1)
+            ]
+        )+self.mat_distance[chromosome[0], chromosome[len(chromosome) - 1]]
+
+    def evaluate(self):
+        '''Cette fonction évalue les individus et attribue une probabilité
+        à chacun d'être sélectionné comme parent.  '''
+        distances = np.asarray(
+            [self.fitness(chromosome) for chromosome in self.sac]
+        )
+
+        self.score = np.min(distances) # stocker le meilleur score d'une generation.
+        self.meilleur = self.sac[distances.tolist().index(self.score)] # stocker l'individu avec le meilleur score.
+        self.parents.append(self.meilleur) # selectionneioné l'individu avec le meilleur score comme un parent.
+        if False in (distances[0] == distances):
+            # on recherche le score le plus élevé et nous soustrayons de celui-ci les valeurs des autres scores,
+            # et nous stockons les résultats dans le même tableau en gardant les mêmes indices.
+            distances = np.max(distances) - distances
+        return distances / np.sum(distances) #ceci renvoie un tableau de probabilités
+
+    def selectionne(self, k):
+        '''Cette fonction sélectionne les parents pour l'étape de crossover'''
+        fit = self.evaluate() #On evalué la generation
+        while len(self.parents) < k: #k représent le nombre de parents a selectioné
+            indice = np.random.randint(0, len(fit)) #on choisit un individu aléatoirement
+            # on génère un nombre aléatoire entre 0 et 1 
+            # et on le compare à la probabilité de l'individu d'être choisi ou non 
+            if fit[indice] > np.random.rand(): 
+                self.parents.append(self.sac[indice])
+        self.parents = np.asarray(self.parents) #nous convertissons la liste des parents en tableau de numpy
+    def crossover(self, p_cross):
+        ''''Cette fonction effectue le crossover où elle sélectionne
+        deux parents et en fonction de la probabilité.'''
+        enfants = []
+        count, taille = self.parents.shape #On stocke la dimension du tableau des parents 
+        for _ in range(len(self.sac)):
+            # on génère un nombre aléatoire entre 0 et 1 et on le compare à la probabilité de crossover 
+            # si le nombre est inférieur à la probabilité on effectue le crossover.
+            if np.random.rand() > p_cross:
+                # Si l'individu n'a pas été choisi, 
+                # on le stock dans la liste des enfants pour la prochaine génération.  
+                enfants.append(
+                    list(self.parents[np.random.randint(count, size=1)[0]])
+                )
+            else:
+                parent_1, parent_2 = self.parents[
+                    np.random.randint(count, size=2), :
+                ]
+                # on génère deux nombres entiers aléatoires entre 0 et le nombre de gènes dans un individu.
+                indice = np.random.choice(range(taille), size=2, replace=False)
+                # nous stockons les deux nombres générés dans deux variables,
+                # le plus petit nombre dans 'debut' et l'autre nombre dans 'fin'.
+                debut, fin = min(indice), max(indice) 
+                enfant = [None] * taille # on crée une liste enfant dont les valeurs sont 'None'.
+                for i in range(debut, fin + 1, 1):
+                    # on remplit l'enfant avec les gènes du premier père en gardant les mêmes indices, 
+                    # on commence à l'indice 'debut' et on s'arrête à 'fin'.
+                    enfant[i] = parent_1[i]
+                pointeur = 0 
+                for i in range(taille):
+                    # maintenant, on remplit le reste avec les gènes manquants du deuxième parent dans le même ordre.
+                    if enfant[i] is None:
+                        while parent_2[pointeur] in enfant:
+                            pointeur += 1
+                        enfant[i] = parent_2[pointeur]
+                enfants.append(enfant)
+        return enfants
+
+
+    def mutate(self, p_cross, p_mut):
+        def swap(chromosome):
+            '''Cette fonction permute les indices de deux villes dans un individu.'''
+            # on génère deux nombres entiers aléatoires entre 0 et le nombre de gènes dans un individu.
+            a, b = np.random.choice(len(chromosome), 2)
+            # on change les indices des gènes dans l'individu  
+            (chromosome[a], chromosome[b]) = (chromosome[b], chromosome[a])
+            return chromosome
+        '''Cette fonction exécute la mutation en utilisant la fonction swap.'''
+        next_sac = [] # on crée une liste vide pour stocker la génération suivante. 
+        enfants = self.crossover(p_cross) # on effectue le crossover sur la génération actuelle.
+        for enfant in enfants:
+            # on génère un nombre aléatoire entre 0 et 1 et on le compare à la probabilité de mutation 
+            # si le nombre est inférieur à la probabilité on effectue le mutation.
+            if np.random.rand() < p_mut:
+                # si la condition est stratifiée on effectue la mutation   
+                next_sac.append(swap(enfant))
+            else:
+                next_sac.append(enfant)
+        self.parents=self.parents.tolist()        
+        return next_sac
+
+
+def genetic_algorithm(
+    villes,
+    mat_distance,
+    nbr_population,
+    nbr_iteration,
+    selectivity=0.2,
+    p_cross=0.7,
+    p_mut=0.1,
+):
+    pop = Population(villes, mat_distance, nbr_population)
+    meilleur = pop.meilleur
+    score = float("inf")
+    histoire = []
+    for i in range(nbr_iteration):
+        pop.selectionne(nbr_population * selectivity)
+        histoire.append(pop.score)
+        if pop.score < score:
+            meilleur = pop.meilleur
+            score = pop.score
+        enfants = pop.mutate(p_cross, p_mut)
+        pop.sac = enfants
+    figure(figsize=(8, 6), dpi=100)   
+    plt.plot(range(len(histoire)), histoire, color="blue",linewidth=1.0)
+    plt.xlabel("Géneration")
+    plt.ylabel("Score")
+    plt.show()  
+    return score, meilleur
